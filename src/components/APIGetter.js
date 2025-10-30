7af29666fdc7410b3c94588eb3ae0061f018a443
Dan <hsammie515@gmail.com>
2025-10-30 12:19:19 +0300

    Взаимодействие с API

---
+++ src/components/APIGetter.js
@@ -0,0 +1,14 @@
+import apiConfig from '../configs/api.json'
+
+export async function getDataByName(name){
+    const {key} = JSON.parse(apiConfig)
+    const fullUrl = `http://www.omdbapi.com/?apikey=${key}&t=${name}`
+
+    const response = await fetch(fullUrl)
+    if(!response.ok)
+        throw new Error('HTTPS Error: ' + response.status + response.statusText)
+    const data = await response.json()
+    if(data.Error)
+        throw new Error('Data Error: ' + data.Error)
+    return data
+}
